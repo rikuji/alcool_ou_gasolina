655b7e5aabfaed5f0e9e81a8c6f0015abb531fdb
rikuji <lucasbezerra1701@gmail.com>
2021-10-13 21:02:38 -0300

    Exibindo resultados

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,50 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(24)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Compensa utilizar álcool",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 40,
+                      fontFamily: 'Big Shoulders Display'),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.all(30),
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(60),
+                  ),
+                  // ignore: deprecated_member_use
+                  child: FlatButton(
+                    child: Text(
+                      "CALCULAR NOVAMENTE",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 25,
+                          fontFamily: 'Big Shoulders Display'),
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
           Input(
             label: "Gasolina",
             ctrl: _gasCtrl,
@@ -38,6 +82,25 @@ class HomePage extends StatelessWidget {
             label: "Álcool",
             ctrl: _alcCtrl,
           ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              child: Text(
+                "CALCULAR",
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 25,
+                    fontFamily: 'Big Shoulders Display'),
+              ),
+              onPressed: () {},
+            ),
+          )
         ],
       ),
     );
